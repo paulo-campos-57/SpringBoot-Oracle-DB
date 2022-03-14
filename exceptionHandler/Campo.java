798d03f7a4308d76eea48ea6fd230c2c33d5f1f2
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-03-14 18:19:39 -0300

    Create Campo.java

---
+++ exceptionHandler/Campo.java
@@ -0,0 +1,12 @@
+package com.fuctura.Client.exceptionHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Campo {
+
+	private String nome;
+	private String mensagem;
+}
